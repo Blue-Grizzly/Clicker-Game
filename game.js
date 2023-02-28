2e6767d464a4a031d9fdfe82b69f4b8338eb8cee
Blue-Grizzly <123905990+Blue-Grizzly@users.noreply.github.com>
2023-02-28 09:18:25 +0100

    lots of improvements
    general improvements to the code
    added path randomization

--- game.js
+++ game.js
@@ -1,75 +1,129 @@
 "use strict";
-
 window.addEventListener("load", start);
 
-
 let points = 0;
-let Health = 3;
-let GameTime = setTimeout(Level_Complete, 10000);
+let HealthPoints = 0;
+// let GameTime = setTimeout(Level_Complete, 10000);
 
-function start(){
+function start() {
+    console.log("start");
 
     points = 0;
-    Health = 3;
-    document.querySelector("#APC1").
-        addEventListener("mousedown", ClickFunction1);
-    document.querySelector("#APC2").
-        addEventListener("mousedown", ClickFunction2);
-    document.querySelector("#RedCross").
-        addEventListener("mousedown", ClickFunction3);
-    document.querySelector("#PeaceKeeper").
-        addEventListener("mousedown", ClickFunction4);
-    console.log("start");
+    HealthPoints = 3;
+
+    listeners();
+    animationStarter();
 }
 
-function ClickFunction1(){
-    console.log("click");
-    document.querySelector("#APC1").classList.
-        add("pause");
-    document.querySelector("#APC_sprite_G").
-        classList.add("clicked");
-    pointplus();
-    console.log("click2");
+
+
+function animationStarter(){
+
+    let pos1 = Math.floor(Math.random()*4+1);
+    let pos2 = Math.floor(Math.random()*4+1);
+    let pos3 = Math.floor(Math.random()*4+1);
+    let pos4 = Math.floor(Math.random()*4+1);
+
+
+
+    document.querySelector("#APC1_container").
+        classList.add("movetest"+pos1);
+    document.querySelector("#APC2_container").
+        classList.add("movetest"+pos2);
+    document.querySelector("#RedCross_container").
+        classList.add("movetest"+pos3);
+    document.querySelector("#PeaceKeeper_container").
+        classList.add("movetest"+pos4);
 }
 
-function ClickFunction2(){
-    console.log("click");
-    document.querySelector("#APC2").classList.
-        add("pause");
-    document.querySelector("#APC_sprite_T").
-        classList.add("clicked");
-    pointplus();
-    console.log("click2");
+function listeners(){
+document
+    .querySelector("#APC1_container")
+    .addEventListener("click", GoodClicked);
+document
+    .querySelector("#APC2_container")
+    .addEventListener("click", GoodClicked);
+document
+    .querySelector("#RedCross_container")
+    .addEventListener("click", BadClicked);
+document
+    .querySelector("#PeaceKeeper_container")
+    .addEventListener("click", BadClicked);
+
+
+    document
+    .querySelector("#APC1_container")
+    .addEventListener("animationiteration", ObjectReset);
+document
+    .querySelector("#APC2_container")
+    .addEventListener("animationiteration", ObjectReset);
+document
+    .querySelector("#RedCross_container")
+    .addEventListener("animationiteration", ObjectReset);
+document
+    .querySelector("#PeaceKeeper_container")
+    .addEventListener("animationiteration", ObjectReset);
+
 }
 
-function ClickFunction3(){
-    console.log("click");
-    // document.querySelector("#RedCross").classList.
-    //     add("pause");
-    // document.querySelector("#RedCross_sprite").
-    //     classList.add("clicked");
-    // console.log("click2");
+
+
+function GoodClicked() {
+    console.log("Click Good");
+    let Good = this;
+    Good.removeEventListener("click", GoodClicked);
+    Good.classList.add("paused");
+    Good.querySelector("img").classList.add("clicked");
+    Good.addEventListener("animationend", GoodGone);
+    GivePoints();
+  }
+
+  function BadClicked() {
+    console.log("Click Good");
+    let Bad = this;
+    Bad.removeEventListener("click", BadClicked);
+    Bad.classList.add("paused");
+    Bad.querySelector("img").classList.add("clicked");
+    Bad.addEventListener("animationend", BadGone);
     PlayerDamage();
-}
+  }
+  
+  function BadGone() {
+    let Bad = this;
+    Bad.removeEventListener("animationend", BadGone);
+    Bad.querySelector("img").classList.remove("clicked");
+    Bad.classList.remove("paused");
+    Bad.addEventListener("click", BadClicked);
+    ObjectReset.call(this);
+  }
 
-function ClickFunction4(){
-    console.log("click");
-    document.querySelector("#PeaceKeeper").classList.
-        add("pause");
-    document.querySelector("#PeaceKeeper_sprite").
-        classList.add("clicked");
-    console.log("click2");
-    PlayerDamage();
-}
+  function GoodGone() {
+    let Good = this;
+    Good.removeEventListener("animationend", GoodGone);
+    Good.querySelector("img").classList.remove("clicked");
+    Good.classList.remove("paused");
+    Good.addEventListener("click", GoodClicked);
+    ObjectReset.call(this);
+  }
+  
+  function ObjectReset(){
+    let Thing = this;
+    Thing.classList.remove
+    ("movetest1", "movetest2", "movetest3", "movetest4");
+    Thing.offsetWidth;
+    let pos1 = Math.floor(Math.random()*4+1);
+    Thing.classList.add("movetest"+pos1);
+  }
 
-function pointplus(){
+
+function GivePoints(){
     points++;
     console.log(points);
     DisplayPoints();
 }
 
 function PlayerDamage(){
-    Health--;
+    HealthPoints--;
     console.log("damage");
     UpdateHealth();
     CheckHealth();
@@ -83,22 +137,24 @@ function DisplayPoints(){
 }
 
 function UpdateHealth(){
-    let HealthPoints = `You have ${Health} Lives left`;
-    console.log(Health);
-    document.querySelector("#health").textContent = HealthPoints;
+    console.log(HealthPoints);
+    if(HealthPoints == 2){
+        document.querySelector("#health1").classList.add("hidden");
+    } else if(HealthPoints == 1){
+        document.querySelector("#health2").classList.add("hidden");
+    } else{
+        document.querySelector("#health3").classList.add("hidden");
+    }
 }
 
 function CheckHealth(){
-    if (Health <= 0){
+    if (HealthPoints <= 0){
         GameOver();
     }
-
 }
 
-
-
 function GameOver(){
-    document.querySelector(".game").
+    document.querySelector("#game_id").
         classList.add("hidden");
     window.clearTimeout(GameTime);
     console.log("Game Over");
@@ -106,8 +162,8 @@ function GameOver(){
 }
 
 function Level_Complete(){
-    document.querySelector(".game").
-        classList.add("background");
+    document.querySelector("#game_id").
+        classList.add("hidden");
     document.querySelector("#LevelComplete").
         classList.add("Level_Complete");
     console.log("Level Complete");
