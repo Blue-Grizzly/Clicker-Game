3295733c86ab8bafd2d980b159316e79b8bdb892
Blue-Grizzly <123905990+Blue-Grizzly@users.noreply.github.com>
2023-03-02 22:21:03 +0100

    It is what its 2

--- game.js
+++ game.js
@@ -260,18 +260,12 @@ function startTimer() {
 }
 
 function Showinstructions(){
-    let instructiontext1 = "Destroy the APCs before the time runs out"
-    let instructiontext2 = "Destroying 1 APC grants 1 point"
-    let instructiontext3 = "But be careful not to hit the PeaceKeepers or the Red Cross"
-    let instructiontext4 = "Clicking the Peace Keeper or the Red Cross will lose you 1 HP"
-    let instructiontext5 = "Failing to click an enemy before it passes through will also cost 1 HP"
-    let instructiontext6 = "Good luck!"
-    document.querySelector("#instructionsBox1").textContent = instructiontext1
-    document.querySelector("#instructionsBox2").textContent = instructiontext2
-    document.querySelector("#instructionsBox3").textContent = instructiontext3
-    document.querySelector("#instructionsBox4").textContent = instructiontext4
-    document.querySelector("#instructionsBox5").textContent = instructiontext5
-    document.querySelector("#instructionsBox6").textContent = instructiontext6
+    document.querySelector("#instructionsBox1").textContent = "Destroy the APCs before the time runs out";
+    document.querySelector("#instructionsBox2").textContent = "Destroying 1 APC grants 1 point";
+    document.querySelector("#instructionsBox3").textContent = "But be careful not to hit the PeaceKeepers or the Red Cross";
+    document.querySelector("#instructionsBox4").textContent = "Clicking the Peace Keeper or the Red Cross will lose you 1 HP";
+    document.querySelector("#instructionsBox5").textContent = "Failing to click an enemy before it passes through will also cost 1 HP";
+    document.querySelector("#instructionsBox6").textContent = "Good luck!";
 }
 
 function StopGame(){
